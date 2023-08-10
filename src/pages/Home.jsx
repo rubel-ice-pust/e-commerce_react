0615456716cb3729b9ef9c874c926f739c4f3ac0
rubel-ice-pust <rubel.pust06@gmail.com>
2023-08-11 03:06:33 +0600

    Basic setupTests

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+    height: 60px;
+`;
+const Wrapper = styled.div`
+      padding: 10px 20px;
+      display: flex;
+      justify-content: space-between;
+`;
+const Left = styled.div`
+flex: 1;
+`;
+
+const Language = styled.span`
+      font-size: 14px;
+      cursor: pointer;
+`;
+const Center = styled.div`
+flex: 1;
+
+`
+const Right = styled.div`
+flex: 1;
+`
+
+
+const Home = () => {
+  return (
+    
+    <Container>
+       <Wrapper>
+         <Left>
+            <Language>EN</Language>
+         </Left>
+         <Center>Center</Center>
+         <Right>Center</Right>
+       </Wrapper>
+    </Container>
+  )
+}
+
+export default Home
