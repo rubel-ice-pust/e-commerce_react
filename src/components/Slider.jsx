5df72e9ddea8619d8f293af0187273600e1e603a
rubel-ice-pust <rubel.pust06@gmail.com>
2023-08-13 03:40:13 +0600

    bodySection with dynamic Image

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,10 @@
+import React from 'react';
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import { styled } from "styled-components"
+import Banner from '../image/img3.jpg';
+import { useState } from 'react';
+
+
 
 
 const Container = styled.div`
@@ -7,6 +12,7 @@ const Container = styled.div`
       height: 100vh;
       display: flex;
       position: relative;
+      overflow: hidden;
      
 
 `;
@@ -22,47 +28,104 @@ const Arrow = styled.div`
       position: absolute;
       top: 0;
       bottom: 0;
-      left: ${props=> props.direction === "left" && "10px"};
-      right: ${props=> props.direction === "right" && "10px"};
+      left: ${(props) => props.direction === "left" && "10px"};
+      right:${(props) =>props.direction === "right" && "10px"};
       margin: auto;
       cursor: pointer;
       opacity: 0.5;
+      z-index: 2;
 
 `;
 const Wrapper = styled.div`
       height:100%;
+      display: flex;
+      transform: translateX(0vw);
 `;
 const Slide = styled.div`
+      width: 100vw;
+      height: 100vh;
       display: flex;
       align-items: center;
+      background-color: #${props=>props.bg};
+
 
 `;
 const ImgContainer = styled.div`
+      height: 100%;
       flex: 1;
 
 `;
-const Image = styled.div``;
+const Image = styled.img`
+      height: 80%;
+
+`;
+const Title = styled.h1`
+      font-size: 20px;
+
+`;
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    letter-spacing: 3px;  
+`;
+const Button = styled.button`
+      padding: 10px;
+      font-size: 20px;
+      background-color: transparent;
+      cursor: pointer;
+`;
 
 const InfoContainer = styled.div`
       flex: 1;
+      padding: 50px;
 
 `;
 
  
  
 const Slider = () => {
+      const [slideIndex, setSlideIndex] = useState(0);
+      const handleClick = (direction) =>{};
+
   return (
      <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onlClick={()=>handleClick("left")}>
         <ArrowLeftOutlined/>
         </Arrow >
         <Wrapper>
+         <Slide bg="f5fafd">
             <ImgContainer>
-            <Image src=" " />
+            <Image src={Banner} />
             </ImgContainer>
-            <InfoContainer></InfoContainer>
+            <InfoContainer>
+               <Title>SUMMER SALE</Title>
+               <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+               <Button>SHOP NOW</Button>
+            </InfoContainer>
+         </Slide>
+         <Slide bg="fcfled">
+            <ImgContainer>
+            <Image src={Banner} />
+            </ImgContainer>
+            <InfoContainer>
+               <Title>WINTER SALE</Title>
+               <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+               <Button>SHOP NOW</Button>
+            </InfoContainer>
+         </Slide>
+         <Slide bg="fbf0f4">
+            <ImgContainer>
+            <Image src={Banner} />
+            </ImgContainer>
+            <InfoContainer>
+               <Title>POPULAR SAL</Title>
+               <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+               <Button>SHOP NOW</Button>
+            </InfoContainer>
+         </Slide>
         </Wrapper>
-        <Arrow direction="right">
+       
+        <Arrow direction="right" onlClick={()=>handleClick("right")}>
         <ArrowRightOutlined/>
         </Arrow>
 
